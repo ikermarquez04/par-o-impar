aa615ad745342d31413a20adcae4db917cd082dd
ikerm <ikerm@LAPTOP-155AQCB8>
2023-01-10 07:47:11 -0600

    Agregar archivos de proyecto.

---
+++ par o impar/par o impar.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero;
+
+    cout << "digite un numero: ";
+    cin >> numero;
+
+    if (numero == 0)
+    {
+    cout << "el numero es cero";
+    }
+
+    else if (numero % 2 == 0) 
+    {
+        cout << "el numero es par";
+    }
+
+    else
+    {
+        cout << "el numero es impar";
+    }
+}
